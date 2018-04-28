64af611316ed495e4892da624ee6c8cedcf53856
DooWhop <liudingpo@qq.com>
2018-04-28 10:58:21 +0800

    code snippet init commit

---
+++ src/main/java/com/doowhop/snippet/MyApplication.java
@@ -0,0 +1,32 @@
+package com.doowhop.snippet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@EnableScheduling
+@SpringBootApplication
+@PropertySource(value = {
+		"file:/app/thfd/conf/java-snippet.properties"
+		}, ignoreResourceNotFound = true)
+public class MyApplication {
+	
+	// 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
+	@Autowired
+	private RestTemplateBuilder builder;
+
+	// 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
+	@Bean
+	public RestTemplate restTemplate() {
+		return builder.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);		
+	}
+}
